c7b34a00018143b824446e857456b230b0ccfb1e
Emandhal <emandhal.exc@gmail.com>
2021-12-26 15:45:05 +0100

    Update SPI_Interface

--- SPI_Interface.h
+++ SPI_Interface.h
@@ -39,6 +39,11 @@
 //=============================================================================
 
 //-----------------------------------------------------------------------------
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+//-----------------------------------------------------------------------------
 #include "ErrorsDef.h"
 //-----------------------------------------------------------------------------
 #ifdef __cplusplus
@@ -106,7 +111,7 @@ typedef struct
 {
   SPI_Conf Config;    //! Configuration of the SPI transfer
   uint8_t ChipSelect; //! Is the Chip Select index to use for the SPI/Dual-SPI/Quad-SPI transfer
-  uint8_t DummyByte;  //! Is the byte to use for receiving data (used with flag SPI_USE_DUMMYBYTE_FOR_RECEIVE in SPIInterface_Packet.Config when receiving data)
+  uint8_t DummyByte;  //! Is the byte to use for receiving data (used with flag SPI_USE_DUMMYBYTE_FOR_RECEIVE in SPIInterface_Packet.Config when receiving data or SPIInterface_Packet.TxData is NULL)
   uint8_t *TxData;    //! Is the data to send through the interface (used with flag SPI_USE_TXDATA_FOR_RECEIVE in SPIInterface_Packet.Config when receiving data)
   uint8_t *RxData;    //! Is where the data received through the interface will be stored. This parameter can be nulled by the driver if no received data is expected
   size_t DataSize;    //! Is the size of the data to send and receive through the interface
@@ -169,12 +174,41 @@ typedef struct
 // SPI Interface functions definitions
 //********************************************************************************************************************
 
-//! SPI bit width enumerator
+#define SPI_PIN_COUNT_Pos         0
+#define SPI_PIN_COUNT_Mask        (0x1Fu << SPI_PIN_COUNT_Pos)
+#define SPI_PIN_COUNT_SET(value)  (((uint16_t)(value) << SPI_PIN_COUNT_Pos) & SPI_PIN_COUNT_Mask) //!< Set data pin count of the SPI communication
+#define SPI_PIN_COUNT_GET(value)  (((uint16_t)(value) & SPI_PIN_COUNT_Mask) >> SPI_PIN_COUNT_Pos) //!< Get data pin count of the SPI communication
+
+#define SPI_CPOL_Pos         7
+#define SPI_CPOL_Mask        (0x1u << SPI_CPOL_Pos)
+#define SPI_CPOL_SET(value)  (((uint16_t)(value) << SPI_CPOL_Pos) & SPI_CPOL_Mask) //!< Set clock polarity
+#define SPI_CPOL_GET(value)  (((uint16_t)(value) & SPI_CPOL_Mask) >> SPI_CPOL_Pos) //!< Get clock polarity
+#define SPI_CPHA_Pos         6
+#define SPI_CPHA_Mask        (0x1u << SPI_CPHA_Pos)
+#define SPI_CPHA_SET(value)  (((uint16_t)(value) << SPI_CPHA_Pos) & SPI_CPHA_Mask) //!< Set clock phase
+#define SPI_CPHA_GET(value)  (((uint16_t)(value) & SPI_CPHA_Mask) >> SPI_CPHA_Pos) //!< Get clock phase
+
+#define SPI_COMM_MODE_Mask  ( SPI_CPOL_Mask | SPI_CPHA_Mask )
+#define SPI_COMM_MODE0      ( SPI_CPOL_SET(0) | SPI_CPHA_GET(0) ) //! SPI mode 0: Clock polarity (CPOL/CKP) = 0 ; Clock phase (CPHA) = 0 ; Clock edge (CKE/NCPHA) = 1
+#define SPI_COMM_MODE1      ( SPI_CPOL_SET(0) | SPI_CPHA_GET(1) ) //! SPI mode 0: Clock polarity (CPOL/CKP) = 0 ; Clock phase (CPHA) = 1 ; Clock edge (CKE/NCPHA) = 0
+#define SPI_COMM_MODE2      ( SPI_CPOL_SET(1) | SPI_CPHA_GET(0) ) //! SPI mode 0: Clock polarity (CPOL/CKP) = 1 ; Clock phase (CPHA) = 0 ; Clock edge (CKE/NCPHA) = 1
+#define SPI_COMM_MODE3      ( SPI_CPOL_SET(1) | SPI_CPHA_GET(1) ) //! SPI mode 0: Clock polarity (CPOL/CKP) = 1 ; Clock phase (CPHA) = 1 ; Clock edge (CKE/NCPHA) = 0
+
+//! SPI bit width and mode enumerator
 typedef enum
 {
-  SPI_MODE      = 0x01, //!< Communication with device with 1 bit per clock (standard SPI)
-  DUAL_SPI_MODE = 0x02, //!< Communication with device with 2 bits per clock (Dual-SPI)
-  QUAD_SPI_MODE = 0x04, //!< Communication with device with 4 bits per clock (Quad-SPI)
+  SPI_MODE0      = SPI_COMM_MODE0 | SPI_PIN_COUNT_SET(1), //!< Communication with device with 1 bit per clock (Standard SPI mode 0)
+  SPI_MODE1      = SPI_COMM_MODE1 | SPI_PIN_COUNT_SET(1), //!< Communication with device with 1 bit per clock (Standard SPI mode 1)
+  SPI_MODE2      = SPI_COMM_MODE2 | SPI_PIN_COUNT_SET(1), //!< Communication with device with 1 bit per clock (Standard SPI mode 2)
+  SPI_MODE3      = SPI_COMM_MODE3 | SPI_PIN_COUNT_SET(1), //!< Communication with device with 1 bit per clock (Standard SPI mode 3)
+  DUAL_SPI_MODE0 = SPI_COMM_MODE0 | SPI_PIN_COUNT_SET(2), //!< Communication with device with 2 bits per clock (Dual-SPI mode 0)
+  DUAL_SPI_MODE1 = SPI_COMM_MODE1 | SPI_PIN_COUNT_SET(2), //!< Communication with device with 2 bits per clock (Dual-SPI mode 1)
+  DUAL_SPI_MODE2 = SPI_COMM_MODE2 | SPI_PIN_COUNT_SET(2), //!< Communication with device with 2 bits per clock (Dual-SPI mode 2)
+  DUAL_SPI_MODE3 = SPI_COMM_MODE3 | SPI_PIN_COUNT_SET(2), //!< Communication with device with 2 bits per clock (Dual-SPI mode 3)
+  QUAD_SPI_MODE0 = SPI_COMM_MODE0 | SPI_PIN_COUNT_SET(4), //!< Communication with device with 4 bits per clock (Quad-SPI mode 0)
+  QUAD_SPI_MODE1 = SPI_COMM_MODE1 | SPI_PIN_COUNT_SET(4), //!< Communication with device with 4 bits per clock (Quad-SPI mode 1)
+  QUAD_SPI_MODE2 = SPI_COMM_MODE2 | SPI_PIN_COUNT_SET(4), //!< Communication with device with 4 bits per clock (Quad-SPI mode 2)
+  QUAD_SPI_MODE3 = SPI_COMM_MODE3 | SPI_PIN_COUNT_SET(4), //!< Communication with device with 4 bits per clock (Quad-SPI mode 3)
 } eSPIInterface_Mode;
 
 //-----------------------------------------------------------------------------
@@ -188,7 +222,7 @@ typedef struct SPI_Interface SPI_Interface; //! Typedef of SPI_Interface device 
  * This function will be called at driver initialization to configure the interface driver
  * @param[in] *pIntDev Is the SPI interface container structure used for the interface initialization
  * @param[in] chipSelect Is the Chip Select index to use for the SPI/Dual-SPI/Quad-SPI initialization
- * @param[in] mode Is the mode of the SPI to configure (0 or 1 -> SPI ; 2 -> Dual-SPI ; 4 -> Quad-SPI)
+ * @param[in] mode Is the mode of the SPI to configure
  * @param[in] sckFreq Is the SCK frequency in Hz to set at the interface initialization
  * @return Returns an #eERRORRESULT value enum
  */
